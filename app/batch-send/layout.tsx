99eacc8883d118c7d08bf51065d78142fe9687bb
Sukanto Das <sukanto01899@gmail.com>
2025-09-18 00:22:48 +0600

    analytics added

--- app/batch-send/layout.tsx
+++ app/batch-send/layout.tsx
@@ -1,5 +1,11 @@
 import AppFooter from "~~/components/stake/footer/Footer";
 import AppHeader from "~~/components/stake/header/Header";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+export const metadata = getMetadata({
+  title: "Monbux - Batch sender",
+  description: "Send MON to multiple addresses in one transaction using Monbux",
+});
 
 const BatchSendLayout = ({ children }: { children: React.ReactNode }) => {
   return (
--- app/swap/layout.tsx
+++ app/swap/layout.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import AppFooter from "~~/components/stake/footer/Footer";
 import AppHeader from "~~/components/stake/header/Header";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+export const metadata = getMetadata({
+  title: "Monbux - Swap token",
+  description: "Easily swap or trade token using Monbux",
+});
 
 const SwapLayout = ({ children }: { children: React.ReactNode }) => {
   return (
