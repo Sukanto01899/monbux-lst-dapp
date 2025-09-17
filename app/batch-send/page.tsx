5c28345e22adcdc5832962604e68d1e633b5fb6e
Sukanto Das <sukanto01899@gmail.com>
2025-09-18 00:13:58 +0600

    batch-sender error fix

--- app/batch-send/page.tsx
+++ app/batch-send/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { NextPage } from "next";
-import BatchEthSender from "~~/components/batch-sender/BatchSend";
 import AllPasteBatchEthSender from "~~/components/batch-sender/DirectBatchSender";
 
 const BatchSenderPage: NextPage = () => {
   return (
     <>
-      {/* <BatchEthSender /> */}
       <AllPasteBatchEthSender />
     </>
   );
